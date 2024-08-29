b13daecde2dd1e2149ac6f45f1e4cad9e2d93cc8
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-08-29 10:31:17 -0400

    first commit

---
+++ 01_getting_started/01_01_python_org.py
@@ -0,0 +1,8 @@
+print("hello World!")
+
+"""
+I think that it is cool that one single coding language has been used for multiple different popular sites.
+I also like that there are so many different resources to get people started and learning python instead of just one straight line of learning.
+
+"""
+
---
+++ 01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,11 @@
+# this will be a typo
+
+'''
+time_of_day = 5f4
+
+doesnt work
+'''
+
+time_of_day = 5
+
+
