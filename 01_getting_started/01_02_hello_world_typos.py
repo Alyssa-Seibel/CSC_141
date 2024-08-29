64f4009efe119ef409bbdf666fae3a6b7f7e7124
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-08-29 10:38:32 -0400

    first commit

--- 01_getting_started/01_02_hello_world_typos.py
+++ 01_getting_started/01_02_hello_world_typos.py
@@ -8,4 +8,17 @@ doesnt work
 
 time_of_day = 5
 
+print "hello world"
+
+'''
+this issue is that i did not put parenthesis before and after the line of text. 
+'''
+
+time of day = 5
+
+'''
+the issue here is that you cannot have spaces in your code. 
+
+'''
+
 
