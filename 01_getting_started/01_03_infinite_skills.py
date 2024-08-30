803525c2c9e7dfeda1e81cdc84c4201481393d65
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-08-30 09:30:06 -0400

    Add files via upload

---
+++ 01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,10 @@
+
+"""
+If I had infinite programing skills im not sure what I would build. 
+Having a large variety is actually harder for me to choose from than a small variety. 
+
+Things I do want to learn to create is my own website. 
+I have been working my way up to selling my jewelry so id love to learn how to make my own website.
+I also would like to create a portfolio for my jewelry. Lastly, i would like to code more games.
+I really enjoyed the Papa's games, so I want to make a game that could be similar to them. 
+"""
