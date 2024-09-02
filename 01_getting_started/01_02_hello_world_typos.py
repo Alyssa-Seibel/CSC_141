34e5c1341bbfbcc189ecebe8616fcbcb48ac2228
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-01 23:34:59 -0400

    Add files via upload

---
+++ 01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,14 @@
+
+#this will be a typo
+# print ("hello world") has no issues
+print "hello world" # creates an issue because there are no parethesis 
+
+# fixing the issue
+
+print ("hello world!")
+
+#When running this code, it tells you there are missing parentheses in call to 'print. It tells you to change it to print()
+
+
+
+
