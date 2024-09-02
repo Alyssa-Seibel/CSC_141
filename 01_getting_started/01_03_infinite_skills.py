5e9c19bc17dc2ac5c7b2cffc1c5358f57c82d4f9
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-01 23:37:26 -0400

    Update 01_03_infinite_skills.py

--- 01_getting_started/01_03_infinite_skills.py
+++ 01_getting_started/01_03_infinite_skills.py
@@ -1,10 +1,11 @@
 
 """
-If I had infinite programing skills im not sure what I would build. 
-Having a large variety is actually harder for me to choose from than a small variety. 
+If I had infinite programing skills, I’m not sure what I would build. 
+Having a large variety is harder for me to choose from than a small variety. 
 
 Things I do want to learn to create is my own website. 
-I have been working my way up to selling my jewelry so id love to learn how to make my own website.
-I also would like to create a portfolio for my jewelry. Lastly, i would like to code more games.
-I really enjoyed the Papa's games, so I want to make a game that could be similar to them. 
+I have been working my way up to selling my jewelry, so I’d love to learn how to make my own website.
+I also would like to create a portfolio for my jewelry. Lastly, I would like to code more games.
+I really enjoyed the “Papa’s” games, so I want to make a game that could be like them.
+
 """
