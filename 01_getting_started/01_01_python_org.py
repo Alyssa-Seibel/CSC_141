64a28415febaf4588d6337e841d70ec08cfc4be2
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-01 23:29:08 -0400

    Add files via upload

---
+++ 01_getting_started/01_01_python_org.py
@@ -0,0 +1,15 @@
+print("hello World!")
+
+
+#I think that it is cool that one single coding language has been used for multiple different popular sites.
+#I also like that there are so many different resources to get people started and learning python instead of just one straight line of learning.
+#I think it is also cool that it lists a bunch of different books about python for people to read and learn from.
+#I like that it also has a list of events that are coming up on the python website.
+#It is also cool that they have a way to help people find jobs 
+
+
+message = "Hello Python World"
+print(message)
+
+message = "hello python crash course world!"
+print(message)
