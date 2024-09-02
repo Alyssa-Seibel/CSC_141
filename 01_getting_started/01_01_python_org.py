e90e0e8472e377df792f1c486bb5f10a8bbdc4a6
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-01 23:28:55 -0400

    Delete 01_getting_started/01_01_python_org.py

--- 01_getting_started/01_01_python_org.py
+++
@@ -1,8 +0,0 @@
-print("hello World!")
-
-"""
-I think that it is cool that one single coding language has been used for multiple different popular sites.
-I also like that there are so many different resources to get people started and learning python instead of just one straight line of learning.
-
-"""
-
