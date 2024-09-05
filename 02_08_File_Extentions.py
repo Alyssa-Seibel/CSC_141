19989faa1a115952f33aad5d5ee331a15b20b5d7
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-05 09:43:36 -0400

    Delete 02_08_File_Extentions.py

--- 02_08_File_Extentions.py
+++
@@ -1,9 +0,0 @@
-filename = 'python_notes.txt'
-
-#removeprefix() removes the prefix of whatever you ask it to remove
-#removesuffix() removes the suffix of whatever you tell it to remove
-
-#first you have to create a new variable to remove the suffix from
-simple_url = filename.removesuffix('.txt')
-
-print(simple_url)
