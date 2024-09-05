e8ae4aa12dcdee2724d7e25d503ee02fd6009553
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-05 11:47:51 -0400

    Add files via upload

---
+++ 02_variables_and_data_types/02_12_Zen_of_Python.py
@@ -0,0 +1,8 @@
+import this
+
+# Things I took from this message was that there seem to always be worse things. Just like trying is better than nothing.
+# You should never not give yourself credit for your work or trials.
+# I also took that you shouldnt give up so easily. 
+# When it says, "Errors should never pass silently" I took that an error shouldnt left without atleast trying to fix it.
+# Also, I saw that it says there usually is one way to do something. Following that it states that "it may not be obvious at first"
+# to me this is reffering to problem solving which ties to the previous quote I mentioned
