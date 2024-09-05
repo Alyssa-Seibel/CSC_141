7aec8ae84d25d057f3219606e10de7e6bb750148
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-05 09:43:22 -0400

    Delete 02_05_Famous_Quote.py

--- 02_05_Famous_Quote.py
+++
@@ -1,5 +0,0 @@
-quote = "You don't need to or should not feel like a victim. Nobody can help you more than you can help yourself."
-first_name = "Lady"
-last_name = "Gaga"
-full_name = f"{first_name} {last_name}"
-print(f'{full_name.title()} once said, "You don\'t need to or should not feel like a victim. Nobody can help you more than you can help yourself."')
