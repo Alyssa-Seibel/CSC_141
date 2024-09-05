e259af9338c519dc04c82e10a4962d3baaa2fba6
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-09-05 09:43:29 -0400

    Delete 02_06_Famous_Quote_2.py

--- 02_06_Famous_Quote_2.py
+++
@@ -1,4 +0,0 @@
-message = '"You don\'t need to or should not feel like a victim. Nobody can help you more than you can help yourself."'
-famous_person = "Lady Gaga"
-print(f'{famous_person.title()} once said, {message}')
-
