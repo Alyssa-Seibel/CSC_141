c3b02b66a6f93cfbb0a5b5fd7be24c875b3f4970
Alyssa-Seibel <alyssa.seibel001@albright.edu>
2024-11-17 15:20:19 -0500

    Add files via upload

---
+++ 12_a_ship_that_fires_bullets/setting_12_05.py
@@ -0,0 +1,10 @@
+
+class Settings:
+    '''store settings'''
+
+    def __init__(self):
+        '''initialize the game's settings'''
+
+        self.screen_width = 600
+        self.screen_height = 400
+        self.bg_color = (40,160,200)
